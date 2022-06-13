2dc965a97d10607e830c867dc0c3eec203933a8a
Vinicius Gabriel <68884985+ViniGab@users.noreply.github.com>
2022-06-13 15:29:17 -0300

    pt 3 13/06/2022
    pt 3 13/06/2022

--- jsBasico/script.js
+++ jsBasico/script.js
@@ -1,9 +1,4 @@
-document.getElementById("titulo").innerHTML = "Opa, tudo bem?";
-
-document.write("Algum Texto"); // Só funciona quando junto do código especial.
-
-window.alert("Mensagem de exemplo") // Quem ta mostrando a mensagem é a janela do navegador e não a página.
-
-console.log("A mensagem inserida aqui aparecerá no console.")
-
-prompt("Digite um número.")
+var hora = 9;
+if ( hora < 12 ) {
+    console.log("Bom dia");
+}
