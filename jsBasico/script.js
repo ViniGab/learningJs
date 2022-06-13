3da5afc979e1042615aabbdc08905e8d99be2ed7
Vinicius Gabriel <68884985+ViniGab@users.noreply.github.com>
2022-06-13 15:03:11 -0300

    pt2 13/06/2022
    pt2 13/06/2022

--- jsBasico/script.js
+++ jsBasico/script.js
@@ -1,1 +1,9 @@
-alert("alerta, estou funcionando!")
+document.getElementById("titulo").innerHTML = "Opa, tudo bem?";
+
+document.write("Algum Texto"); // Só funciona quando junto do código especial.
+
+window.alert("Mensagem de exemplo") // Quem ta mostrando a mensagem é a janela do navegador e não a página.
+
+console.log("A mensagem inserida aqui aparecerá no console.")
+
+prompt("Digite um número.")
