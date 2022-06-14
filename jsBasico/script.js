21696f873ee8120fb46a7b83ef7c19b58191afd5
Vinicius Gabriel <68884985+ViniGab@users.noreply.github.com>
2022-06-14 02:26:38 -0300

    pt 2 14/06/2022
    pt 2 14/06/2022

--- jsBasico/script.js
+++ jsBasico/script.js
@@ -1,7 +1,3 @@
-function clicou() {
-    alert("Você clicou no botão!");
-}
-
-function clicou1() {
-    document.getElementById("titulo").innerHTML = "Obrigado por logar ;)"
+function digitou(e) {
+    console.log(e);
 }
