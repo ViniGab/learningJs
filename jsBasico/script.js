7a9ade4fc71cb4eaaf73bdab99c297d3b86f1285
Vinicius Gabriel <68884985+ViniGab@users.noreply.github.com>
2022-06-14 13:01:06 -0300

    pt 3 14/06/2022
    pt 3 14/06/2022

--- jsBasico/script.js
+++ jsBasico/script.js
@@ -1,3 +1,13 @@
-function digitou(e) {
-    console.log(e);
+function azul() {
+    document.getElementById("titulo").classList.add('azul');
+}
+
+function vermelho() {
+    document.getElementById("titulo").classList.add('vermelho');
+
+}
+
+function verde() {
+    document.getElementById("titulo").classList.add('verde');
+
 }
