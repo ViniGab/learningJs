93b7f1c6af77f893799404a1d81352383807ec3f
Vinicius Gabriel <68884985+ViniGab@users.noreply.github.com>
2022-06-15 18:54:23 -0300

    pt 2 15/06/2022
    pt 2 15/06/2022

--- jsBasico/script.js
+++ jsBasico/script.js
@@ -4,3 +4,10 @@ document.getElementsByClassName('lista')[0].innerHTML = 'Item alterado!'; // Par
 document.getElementsByTagName('button') // Para as tags.
 document.getElementsByTagName('input') // Para as tags.
 document.getElementsByName('text') // Para os campos de inputs e derivados.
+
+document.querySelector('#procurarpeloID') // ID
+document.querySelector('.lista') // Classes, sempre mostra o primeiro que for achado.
+document.querySelectorAll('.lista') // O mesmo do acima, porém mostra todos os achados.
+document.querySelector('button') // Procura pela tag.
+document.querySelector('button').innerHTML = 'Inscreva-se'; // Alterando o conteúdo dentro do button.
+
