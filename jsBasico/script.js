8ecb21223cb930a7bc86ddd10803d71ea5400f05
Vinicius Gabriel <68884985+ViniGab@users.noreply.github.com>
2022-06-15 18:38:15 -0300

    pt 1 15/06/2022
    pt 1 15/06/2022

--- jsBasico/script.js
+++ jsBasico/script.js
@@ -1,10 +1,6 @@
-let html = '';
-
-let c = 0;
-
-while(c <= 10) {
-    html += "Número: "+c+"<br/>";
-    c++;
-}
-
-
+document.getElementById('Exemplo').innerHTML = 'Pedro'; // Com ID.
+document.getElementsByClassName('lista') // Para classes.
+document.getElementsByClassName('lista')[0].innerHTML = 'Item alterado!'; // Para um valor de classe em específico.
+document.getElementsByTagName('button') // Para as tags.
+document.getElementsByTagName('input') // Para as tags.
+document.getElementsByName('text') // Para os campos de inputs e derivados.
