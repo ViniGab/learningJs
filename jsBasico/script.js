489a788c17229af281282b859acd1ab46a91d4a9
Vinicius Gabriel <68884985+ViniGab@users.noreply.github.com>
2022-06-16 14:02:38 -0300

    pt 1 16/06/2022
    pt 1 16/06/2022

--- jsBasico/script.js
+++ jsBasico/script.js
@@ -1,13 +1,25 @@
-HTML:
+let nome = 'Maria Silva Santos';
 
-<img src="images/cachorro.jpg" data-animal='Cachorro' class="imagem" /><br/>
+console.log(nome.indexOf('Silva')); // Onde a string inicia. 6.  
 
-<button onclick="trocarImagem('cachorro.jpg', 'Cachorro')">Cachorro</button>
-<button onclick="trocarImagem('gato.jpg', 'Gato')">Cachorro</button>
+console.log(nome.slice(10, 15));  // serve para procurar o valor no local indicado, no caso "a San". pode ser usado negativo também.
 
-Script:
+console.log(nome.length)  // Tamanho, 18.
 
-function trocarImagem(filename, animalname) {
-    document.querySelector('.imagem').setAttribute('src', 'images/'+filename);
-    document.querySelector('.imagem').setAttribute('data-animal', animalname);
-}
+console.log(nome.substring(2, 8));  // ria Si
+
+console.log(nome.replace('Silva', 'Rodrigues'));  // Maria Rodrigues Santos, substitui o elemento para vizualização.
+
+// Lembrando que, como o replace acima não foi armazenado na variável principal a mesma continua com o valor anterior.
+
+console.log(nome.toUpperCase());  // MARIA SILVA SANTOS
+console.log(nome.toLowerCase());  // maria silva santos
+console.log(nome.trim()); // Serve para reduzir os espaços do início e final. muito importante para formulários e acessos de usuários.
+console.log(nome.charAt(4)); // Para saber o elemento que está nesta posição informada, no caso "a"
+console.log(nome[4]); // O mesmo do acima, porém melhor utilizado.
+console.log(nome.split(' '));  // separa a string, gera array. [ 'Maria', 'Silva', 'Santos' ].
+// Obs: Caso a string acima estivesse sendo separada com outro simbolo ao invés do espaço, como o ";" ou outro, colocariamos este simbolo.
+
+
+
+
