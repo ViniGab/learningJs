b40a406086197ade231ff26b98558052fc57bf88
Vinicius Gabriel <68884985+ViniGab@users.noreply.github.com>
2022-06-22 14:59:50 -0300

    pt 1 22/06/2022
    pt 1 22/06/2022

--- jsBasico/script.js
+++ jsBasico/script.js
@@ -1,1 +1,20 @@
-'{"nome": "Vitória", "idade": 90}'
+function loadPosts() {
+    document.getElementById("posts").innerHTML = 'Carregando...'
+
+    fetch('https://jsonplaceholder.typicode.com/posts')  // fetch retorna uma promisse
+        .then(function(resultado){
+            return resultado.json();
+        })
+        .then(function(json){
+            document.getElementById("posts").innerHTML = json.length+' posts'
+        })
+        .catch(function(error){
+            console.log("Deu problema!")
+        })
+}
+
+
+/* Obs: logo após o fetch e os parênteses, nós podemos colocar uma chave e definir o método que vai ser utilizado, como Get, Post, etc. 
+O padrão que já vem setado é GET. */
+
+
